966419d96c0c5046fca62a6813afa614e67c25ed
sonle <sonle@geekyants.com>
2022-03-29 18:15:34 +0700

    update at_client

--- at_onboarding_flutter/lib/screen/at_onboarding_start_screen.dart
+++ at_onboarding_flutter/lib/screen/at_onboarding_start_screen.dart
@@ -2,11 +2,13 @@ import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:at_onboarding_flutter/at_onboarding_result.dart';
 import 'package:at_onboarding_flutter/services/onboarding_service.dart';
 import 'package:at_onboarding_flutter/utils/at_onboarding_dimens.dart';
+import 'package:at_onboarding_flutter/widgets/at_onboarding_button.dart';
 import 'package:at_sync_ui_flutter/at_sync_material.dart';
 import 'package:flutter/material.dart';
 
 import '../at_onboarding.dart';
 import '../services/at_onboarding_config.dart';
+import '../widgets/at_onboarding_dialog.dart';
 
 class AtOnboardingStartScreen extends StatefulWidget {
   final AtOnboardingConfig config;
@@ -29,14 +31,23 @@ class _AtOnboardingStartScreenState extends State<AtOnboardingStartScreen> {
   }
 
   void _init() async {
-    final OnboardingService onboardingService =
-        OnboardingService.getInstance();
+    final OnboardingService onboardingService = OnboardingService.getInstance();
+    final isUsingSharedStorage = await onboardingService.isUsingSharedStorage();
+    if (isUsingSharedStorage == null) {
+      //No defind yet
+      final result = await askUserUseSharedStorage();
+      await onboardingService.initialSetup(usingSharedStorage: result);
+    } else {
+      await onboardingService.initialSetup(
+          usingSharedStorage: isUsingSharedStorage);
+    }
     onboardingService.setAtClientPreference = widget.config.atClientPreference;
     try {
       final result = await onboardingService.onboard();
       debugPrint("AtOnboardingInitScreen: result - $result");
       if (!mounted) return;
-      Navigator.pop(context, AtOnboardingResult.success(atsign: onboardingService.currentAtsign!));
+      Navigator.pop(context,
+          AtOnboardingResult.success(atsign: onboardingService.currentAtsign!));
     } catch (e) {
       debugPrint("AtOnboardingInitScreen: error - $e");
       if (e == OnboardingStatus.ATSIGN_NOT_FOUND ||
@@ -82,5 +93,37 @@ class _AtOnboardingStartScreenState extends State<AtOnboardingStartScreen> {
         ),
       ),
     );
+  }
+
+  Future<bool> askUserUseSharedStorage() async {
+    final result = await showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AtOnboardingDialog(
+            title:
+                'Do you want to share this onboarded atsign with other apps on @platform?',
+            message:
+                'This would save you the process to onboard this atsign on other apps again.',
+            actions: [
+              AtOnboardingSecondaryButton(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+              AtOnboardingPrimaryButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          );
+        });
+    if (result is bool) {
+      return result;
+    }
+    return false;
   }
 }
